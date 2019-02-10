3848cb573c11df9ea35bfe5419aa1693a6fa3a93
hukuhuku <hukuhuku11111arm52@gmail.com>
2019-02-10 10:10:15 +0900

    [add]関数ライブラリ

--- libs/bellman_ford.py
+++ libs/bellman_ford.py
@@ -1,19 +1,38 @@
-N,M = map(int,input().split()) #Nは頂点の数、Mは辺の数
-E = [[int(_) for _ in input().split()] for i in range(M)]　# [[a1,b1,c1],[a2,b2,c3]]みたいな感じ,頂点a1から頂点b1までを重さc1で結ぶ
+class BellmanFord:
+    def __init__(self, graph, num_v, start_point):
+        """
+        graph : [[a1,b1,c1],[a2,b2,c2],‥]みたいな3次元配列のリスト,a1からb1までのコストはc1
+        num_v :頂点の数
+        start_point :スタート地点(a1)
+        """
+        self.num_v = num_v
+        self.graph = graph
+        self.start_point = start_point
+ 
+ 
+    def execute(self):
+        dist = [float("inf") for _ in range(self.num_v)]
+        dist[self.start_point] = 0
 
+        negative = [False for _ in range(self.num_v)]
 
-max_value = 99999*99999
-scores = [max_value for i in range(N)]
-scores[0] = 0
+        for i in range(self.num_v - 1):
+            for start, end, cost in self.graph:
+                if dist[end] > dist[start] + cost:
+                    dist[end] = dist[start] + cost
 
-for a,b,c in E:
-    #頂点の添え字を配列の添え字に合わせるために1を引く
-    a -= 1 
-    b -= 1
-    if scores[b] >= scores[a] + c:#E[0]が頂点
-        scores[b] = scores[a] + c
+        #負の経路がないなら|V-1|以上の繰り返しで最短経路の更新は行われない、負の経路の検出
+        for i in range(self.num_v):
+            for start, end, cost in self.graph:
+                if dist[end] > dist[start] + cost:
+                    dist[end] = dist[start] + cost
+                    negative[end] = True
+                if negative[start]:
+                    negative[end] = True
+        return dist, negative
+ 
+ 
 
-print(scores)
 
 
     
